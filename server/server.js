ff8852455a7633495acb44a435805782df16e823
amitlzkpa <amit.lzkpa@gmail.com>
2020-05-12 14:59:45 -0500

    fix mongo url

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 8080;
 const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN;
 const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID;
 
-const mongoURI = `${process.env.MONGO_URL}`;
+const mongoURI = process.env.MONGO_URL;
 mongoose.connect(mongoURI, { useNewUrlParser: true });
 
 const app = express();
