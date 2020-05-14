26e0a34e01eb622efe01f0cf1ae6c67b35b3ba9f
amitlzkpa <amit.lzkpa@gmail.com>
2020-05-13 22:27:35 -0500

    add cookie parser and increase post body size limits

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const DIR = 'dist';
 const PORT = process.env.PORT || 8080;
@@ -15,8 +16,9 @@ mongoose.connect(mongoURI, { useNewUrlParser: true });
 const app = express();
 app.use(express.static(DIR));
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(bodyParser.json({ limit: '500mb' }));
+app.use(bodyParser.urlencoded({ limit: '500mb', extended: true }));
 
 app.get('/api/auth0-secrets', function(req, res) {
   const creds = {
