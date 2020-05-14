840d9cda30f5a224f5f84d4215a78aa4b494aa05
amitlzkpa <amit.lzkpa@gmail.com>
2020-05-14 16:09:53 -0500

    add schema swagger docs for User

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,45 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-// _id is added by default
+
+
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *      User:
+ *        type: object
+ *        required:
+ *          - name
+ *        properties:
+ *          _id:
+ *            type: ObjectId
+ *            description: Unique identifier automatically added to the user.
+ *          name:
+ *            type: String
+ *            default: ""
+ *          email:
+ *            type: String
+ *            default: ""
+ *            description: Email for the user, needs to be unique.
+ *          username:
+ *            type: String
+ *            description: Username for the user, needs to be unique.
+ *          bio:
+ *            type: String
+ *            description: A descriptive bio for the user.
+ *          permissions:
+ *            type: Object
+ *            description: An object representing the true/false permission values for keys.
+ *                         By default contains a permission set for 'admin' set to false.
+ *        example:
+ *           username: pparker
+ *           name: Peter Parker
+ *           email: pp_is_stuck@web.com
+ *           bio: My spidey sense tingles if you don't write docs.
+ *           permissions: { admin: true, read: true, write: false }
+ */
+
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -30,5 +68,7 @@ const UserSchema = new mongoose.Schema({
     }
   }
 }, {timestamps: true});
+
+
 
 module.exports = mongoose.model('User', UserSchema);
