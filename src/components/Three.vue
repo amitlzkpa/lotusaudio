436cf66868e1b5908768fe5b6f0ad44077122d68
amitlzkpa <amit.lzkpa@gmail.com>
2020-06-03 04:43:40 -0500

    make threejs stuff internal

--- src/components/Three.vue
+++ src/components/Three.vue
@@ -7,48 +7,37 @@
 <script>
 import * as THREE from 'three';
 
+let container, renderer, scene, camera;
+
 export default {
   name: 'Three',
   data() {
     return {
-      camera: null,
-      scene: null,
-      renderer: null,
-      container: null,
-      mesh: null
     }
   },
   methods: {
     init: function() {
-      this.container = document.getElementById('container');
+      container = document.getElementById('container');
 
-      this.camera = new THREE.PerspectiveCamera(70, this.container.clientWidth/this.container.clientHeight, 0.01, 10);
-      this.camera.position.z = 1;
+      camera = new THREE.PerspectiveCamera(70, container.clientWidth/container.clientHeight, 0.01, 10);
+      camera.position.z = 1;
 
-      this.scene = new THREE.Scene();
+      scene = new THREE.Scene();
 
-      let geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-      let material = new THREE.MeshNormalMaterial();
-
-      this.mesh = new THREE.Mesh(geometry, material);
-      this.scene.add(this.mesh);
-
-      this.renderer = new THREE.WebGLRenderer({antialias: true});
-      this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
-      this.container.appendChild(this.renderer.domElement);
+      renderer = new THREE.WebGLRenderer({antialias: true});
+      renderer.setSize(container.clientWidth, container.clientHeight);
+      container.appendChild(renderer.domElement);
       
       window.addEventListener('resize', () => {
-        this.camera.aspect = this.container.clientWidth / this.container.clientHeight;
-        this.camera.updateProjectionMatrix();
-        this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
+        camera.aspect = container.clientWidth / container.clientHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(container.clientWidth, container.clientHeight);
       }, false);
 
     },
     animate: function() {
       requestAnimationFrame(this.animate);
-      this.mesh.rotation.x += 0.01;
-      this.mesh.rotation.y += 0.02;
-      this.renderer.render(this.scene, this.camera);
+      renderer.render(scene, camera);
     }
   },
   mounted() {
