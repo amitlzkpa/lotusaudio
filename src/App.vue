f540bb9e594b5538d45c47ab12930c45445c066d
amitlzkpa <amit.lzkpa@gmail.com>
2020-06-03 03:26:30 -0500

    include fontawesome for buefy; remove navbar partial

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
   <div id="app">
-    <Navbar />
+
+    <nav class="level">
+      
+      <div class="level-left">
+        <div class="level-item">
+          <router-link to="/">Home</router-link>
+        </div>
+        <div class="level-item">
+          <router-link to="/about">About</router-link>
+        </div>
+        <div class="level-item">
+          <router-link v-if="$auth.isAuthenticated" to="/profile">Profile</router-link>
+        </div>
+      </div>
+
+      <div class="level-right">
+        <div class="level-item">
+          <span v-if="!$auth.loading">
+            <a v-if="!$auth.isAuthenticated" @click="login" href="#!">Log in</a>
+            <a v-if="$auth.isAuthenticated" @click="logout" href="#!">Log out({{ $auth.user.name }})</a>
+          </span>
+        </div>
+      </div>
+      
+    </nav>
+
     <router-view />
+
   </div>
 </template>
 
 <script>
-import Navbar from '@/partials/Navbar.vue'
 
 export default {
   name: 'App',
   components: {
-    Navbar
+  },
+  methods: {
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin
+      });
+    }
   }
 }
 </script>
